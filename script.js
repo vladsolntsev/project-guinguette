c8d84ac3f4bb3b2b7d9dbbe616e86d7891f12465
Blustery33 <guillaume.moultson@gmail.com>
2020-09-30 11:51:38 +0200

    add desktop navigation + responsive

---
+++ script.js
@@ -0,0 +1,7 @@
+/*  JS Navigation */
+const burger = document.getElementById('burger');
+const menuBurger = document.getElementById('menu-burger')
+burger.addEventListener('click', event => {
+    menuBurger.classList.toggle('display-block')
+})
+/* End JS Navigation */
