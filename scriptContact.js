1a11bae5e9ec47b79db189f7d6420dcea4f63c63
Benjamin Touboul <touboul.benjamin@gmail.com>
2020-10-03 14:04:45 +0200

    Histoire Footer Vidéo Contact Form

--- scriptContact.js
+++ scriptContact.js
@@ -2,20 +2,20 @@ const demandeInformations = document.getElementById('demandeInformations');
 demandeInformations.addEventListener('click', function () {
     demandeInformations.style.backgroundColor = '#C5404D';
     demandeInformations.style.color = 'beige';
-    demandeInformations.style.width = '70%';
+    demandeInformations.style.width = '90%';
     reservations.style.background = 'lightgrey';
     reservations.style.color = '#C5404D';
-    reservations.style.width = '40%';
+    reservations.style.width = '75%';
     }
 );
 const reservations = document.getElementById('reservations');
 reservations.addEventListener('click', function() {
     reservations.style.backgroundColor = '#C5404D';
     reservations.style.color = 'beige';
-    reservations.style.width = '70%';
+    reservations.style.width = '90%';
     demandeInformations.style.backgroundColor = 'lightgrey';
     demandeInformations.style.color = '#C5404D';
-    demandeInformations.style.width = '40%';
+    demandeInformations.style.width = '75%';
 
     }
 );
