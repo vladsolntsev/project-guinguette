d570d0c80cc7dee24153e260736efe5610ed19e7
Benjamin Touboul <touboul.benjamin@gmail.com>
2020-10-07 17:05:08 +0200

    problems with Phil

--- scriptContact.js
+++ scriptContact.js
@@ -2,24 +2,51 @@ const demandeInformations = document.getElementById('demandeInformations');
 demandeInformations.addEventListener('click', function () {
     demandeInformations.style.backgroundColor = '#C5404D';
     demandeInformations.style.color = 'beige';
-    demandeInformations.style.width = '90%';
+    demandeInformations.style.width = '70%';
     reservations.style.background = 'lightgrey';
     reservations.style.color = '#C5404D';
-    reservations.style.width = '75%';
+    reservations.style.width = '45%';
     }
 );
 const reservations = document.getElementById('reservations');
 reservations.addEventListener('click', function() {
     reservations.style.backgroundColor = '#C5404D';
     reservations.style.color = 'beige';
-    reservations.style.width = '90%';
+    reservations.style.width = '70%';
     demandeInformations.style.backgroundColor = 'lightgrey';
     demandeInformations.style.color = '#C5404D';
-    demandeInformations.style.width = '75%';
+    demandeInformations.style.width = '45%';
 
     }
 );
-    const mainButton = document.getElementById("shake");
+
+const shakeShake = document.getElementById('shake');
+shakeShake.onmouseover = shakeMe;
+
+
+    function shakeMe() {
+const mainButton = document.getElementById("shake");
+const firstname = document.getElementById("prenom");
+const lastname = document.getElementById("nom");
+const telephone = document.getElementById("telephone");
+const email = document.getElementById("e-mail");
+const message = document.getElementById("message");
+if (
+    !telephone.checkValidity() ||
+    !firstname.checkValidity() ||
+    !lastname.checkValidity()  ||
+    !telephone.checkValidity() ||
+    !email.checkValidity()     ||
+    !message.checkValidity()
+        ) {
+            mainButton.style.animation = 'shake 0.5s';
+            mainButton.style.animationIterationCount = '2';
+            /*              mainButton.style.backgroundColor = '#621e24';*/
+
+        };
+    };
+
+/*    const mainButton = document.getElementById("shake");
     const firstname = document.getElementById("prenom");
     const lastname = document.getElementById("nom");
     const telephone = document.getElementById("telephone");
@@ -36,8 +63,9 @@ reservations.addEventListener('click', function() {
             ) {
                 mainButton.style.animation = 'shake 0.5s';
                 mainButton.style.animationIterationCount = '2';
-/*              mainButton.style.backgroundColor = '#621e24';*/
+                mainButton.style.backgroundColor = '#621e24';
 
             };
         }
     );
+*/
